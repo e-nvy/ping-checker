a9783965e1423266a9dcd2fd4e69e6f2b26e2a08
e-nvy <tanveergill022@gmail.com>
2022-09-30 09:56:39 +0000

    modify ping cmd

--- avg_rtt.sh
+++ avg_rtt.sh
@@ -1,6 +1,6 @@
 ipfile=ips.txt
 while read IP; do
-	average=`ping -nq -w 1 -i 0.2 -c 100 $IP | grep rtt | cut -d '/' -f 5`
+	average=`ping -c 5 $IP | grep rtt | cut -d '/' -f 5`
 	if [ "X$average" == "X" ]; then
 		continue
 	fi
